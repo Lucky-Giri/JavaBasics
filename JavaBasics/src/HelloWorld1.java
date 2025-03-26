139439a4745095999d595908af4a737c9d108e60
Lucky giri <luckygiri202326@gmail.com>
2025-03-26 22:24:31 +0530

    adding lucky giri

--- JavaBasics/src/HelloWorld1.java
+++ JavaBasics/src/HelloWorld1.java
@@ -1,5 +1,7 @@
 public class HelloWorld1 {
     public static void main (String [] args) {
         System.out.println( " Hello WOrld");
+        System.out.println( "");
+        System.out.println( " LuckyGiri");
     }
 }
