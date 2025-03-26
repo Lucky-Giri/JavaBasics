c1d8560ccb1c0bec03d58d339e9c0adddae6f30f
Lucky giri <luckygiri202326@gmail.com>
2025-03-26 22:28:53 +0530

    #

--- JavaBasics/src/HelloWorld1.java
+++ JavaBasics/src/HelloWorld1.java
@@ -1,7 +1,6 @@
 public class HelloWorld1 {
     public static void main (String [] args) {
         System.out.println( " Hello WOrld");
-        System.out.println( "");
-        System.out.println( " LuckyGiri");
+       
     }
 }
