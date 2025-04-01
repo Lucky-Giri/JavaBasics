8a0d9840329b6ebff79c1fc9b19cc9d0ce15e33d
Lucky giri <luckygiri202326@gmail.com>
2025-04-01 23:59:22 +0530

    hello world program day 1 with three types spacing to next line

---
+++ JavaBasics/src/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main (String args []){
+        System.out.println("Hello World");
+        System.out.print("Hello \n");
+        System.out.println("Hello ");
+        System.out.print("Hello\n");
+        System.out.print("Hello ");
+
+    }
+    
+}
