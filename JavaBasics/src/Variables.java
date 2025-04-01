b59666c3534312843247ebae6cf1738cb653e216
Lucky giri <luckygiri202326@gmail.com>
2025-04-02 00:13:04 +0530

    variables , if i change the values after some time then value is changable

---
+++ JavaBasics/src/Variables.java
@@ -0,0 +1,15 @@
+public class Variables {
+    public static void main (String args[]){
+        int a = 5;
+        int b = 10;
+        String name = "Lucky";
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(name);
+        a = 50;
+        System.out.println(a);
+        a = b;
+        System.out.println(a);
+    }
+    
+}
