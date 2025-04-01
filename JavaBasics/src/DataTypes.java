3d6ad79fa9420f19d51367671ec7f95485152047
Lucky giri <luckygiri202326@gmail.com>
2025-04-02 00:21:52 +0530

    Discuss some Data Types in java

---
+++ JavaBasics/src/DataTypes.java
@@ -0,0 +1,15 @@
+public class DataTypes {
+    public static void main (String args []){
+        byte b = 8;
+        char ch = 'a';
+        boolean var = true;
+        int number = 97;
+        float marks = 99.56f;
+        System.out.println(b);
+        System.out.println(ch);
+        System.out.println(var);
+        System.out.println(number);
+        System.out.println(marks);
+    }
+    
+}
---
+++ JavaBasics/src/tempCodeRunnerFile.java
@@ -0,0 +1,4 @@
+System.out.println(ch);
+        System.out.println(var);
+        System.out.println(number);
+        System.out.println(marks);
